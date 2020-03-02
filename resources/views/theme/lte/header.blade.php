b55c98b0414fe0f7fb9aadf586206c8f6986d67a
YefryMontero <yefry.montero@correounivalle.edu.co>
2020-03-01 23:10:48 -0500

    actualizar Login

---
+++ resources/views/theme/lte/header.blade.php
@@ -0,0 +1,49 @@
+<header class="main-header">
+  <!-- Logo -->
+  <a href="/" class="logo">
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg">Sistema<b>PQR</b></span>
+  </a>
+  <!-- Header Navbar: style can be found in header.less -->
+  <nav class="navbar navbar-static-top">
+
+    <div class="navbar-custom-menu">
+      <ul class="nav navbar-nav">
+        
+        <!-- User Account: style can be found in dropdown.less -->
+        <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="{{asset("assets/$theme/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
+            <span class="hidden-xs">Hola, {{session()->get('nombre_usuario') ?? 'Aun no inicias sesion'}}</span>
+          </a>
+          <ul class="dropdown-menu">
+          
+            <!-- Menu Body -->
+            <li class="user-body">
+              <div class="row">
+                  @if(session()->get("roles") && count(session()->get("roles")) > 1)
+                  <div class="col-xs-6 text-center">
+                      <a href="#" class="cambiar-rol">Cambiar Rol</a>
+                  </div>
+                  @endif
+              </div>
+            </li>
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <div class="pull-left">
+                <a href="{{route('login')}}" class="btn btn-default btn-flat">Login</a>
+              </div>
+              <div class="pull-right">
+                <a href="{{route('logout')}}" class="btn btn-default btn-flat">Salir</a>
+              </div>
+            </li>
+          </ul>
+        </li>
+        <!-- Control Sidebar Toggle Button -->
+        <li>
+          <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
