06ac6b5f354bde30dcfb9e1df96a868de57da687
Yefry Montero <yefry.montero@correounivalle.edu.co>
2020-03-10 18:01:29 -0500

    .

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -8,7 +8,7 @@ class InicioController extends Controller
 {
     public function index()
     {
-        return view('admin.admin.index');
+        return view('inicio');
     }
 
     public function create()
